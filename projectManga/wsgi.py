ea3771d314f1c5835cf270199324c128d140412f
NattapolSurinkeaw <nattapol.surinkeaw@gmail.com>
2024-10-23 23:21:06 +0700

    first commit

---
+++ projectManga/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for projectManga project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectManga.settings')
+
+application = get_wsgi_application()
